b8cb87def2886f1a125d9299d041144e2ba12136
Danilo Gondim <daniloencarnacao@gmail.com>
2020-08-05 22:12:10 +0000

    Create a basic bar chart. Bar heights depend on values. Bar colours are customizable

--- draw-bar-chart.js
+++ draw-bar-chart.js
@@ -1,6 +1,11 @@
+//@ts-nocheck
 const charOptions = {
   width: 200,
-  height: 200
+  height: 200,
+  barColour: "light-blue",
+  labelColour: "black",
+  barSpacing: 20,
+  barChartAxes: ["x", "y"]
 }
 
 
@@ -9,18 +14,27 @@ const drawBarChart = function (data, options, element) {
   let barHeight;
   if (options.width !== undefined) {
     charOptions.width = options.width;
-  }
+  };
   if (options.height !== undefined) {
     charOptions.height = options.height;
   }
+  const destination = document.querySelector("#destination"); //change back to element
+  let counter = 0;
   for (let value of data) {
-    barWidth = charOptions.width / 2 / data.length; // bar width dependens on the total amount of values passed.
+    barWidth = charOptions.width / 2 / data.length; // bar width depends on the total amount of values passed.
     barHeight = value; // bar height should be dependent on the values of the data.
-  }
-  const destination = document.getElementById(element);
-  console.log(data, options, element);
+    destination.insertAdjacentHTML("beforeend",
+      `<span id="bars${counter}" class="bars">${barHeight}</span>`
+    )
+    $(`#bars${counter}` ).css({
+      "height": `${(barHeight/Math.max(...data))*100}%`,
+      "background-color": `${charOptions.barColour}`,
+      "vertical-align": "bottom",
+      "border": "0.1mm solid black"
+    });
+    counter++;
+  };
 };
-
 
 // The data parameter will be the data the chart should work from Start with just an
 // Array of numbers e.g. [1, 2, 3, 4, 5]
